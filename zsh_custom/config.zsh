1f96ed99ee24adf6bb0a8ab64d54eb6fbccb4cb5
Daniel Nilsson <daniel.nilsson1989@gmail.com>
2017-06-24 20:45:13 +0200

    addeed zsh config
    Signed-off-by: Daniel Nilsson <daniel.nilsson1989@gmail.com>

---
+++ zsh_custom/config.zsh
@@ -0,0 +1,17 @@
+
+export GOPATH=$HOME/go
+export PATH=/Users/dln/.composer/vendor/bin:$PATH
+export PATH=$GOPATH/bin:$PATH
+export PATH=$HOME/.dotfiles/bin:$PATH
+export PATH=/usr/local/Cellar/bison/3.0.4/bin:$PATH
+export PATH=/Users/dln/.node/bin:$PATH
+
+
+alias cgs="clear; git status"
+alias lla="ls -la"
+alias pa="php artisan"
+alias devup="cd ~/box/development; vagrant up"
+alias puf="phpunit --verbose --debug --filter="
+alias g="git"
+alias nah="git reset --hard; git clean -df"
+
