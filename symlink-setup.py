7c04d35f4f33606f88ed71c30171e5dec449dd75
Daniel Nilsson <daniel.nilsson1989@gmail.com>
2017-07-12 09:20:00 +0200

    updated symlink setup
    singlefile
    
    Signed-off-by: Daniel Nilsson <daniel.nilsson1989@gmail.com>

--- symlink-setup.py
+++ symlink-setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
     
+import shutil
+import errno
+import ntpath
 import os
 import sys
 import json
 import pprint
-sys.path.insert(0, os.getcwd() + '/src')
-import functions
-
 
 # Base path to symlink from
 home =  os.environ['HOME'] + '/'
@@ -31,5 +31,50 @@ for item in jsonObject['symlinks']:
         functions.force_symlink(file, new_path)
 
 
+def path_leaf(path):
+    head, tail = ntpath.split(path)
+    return tail or ntpath.basename(head)
 
+def select_files(root, files):
+    """
+    select all files in root directory
+    root  - root directory
+    files - files
+    """
+    selected_files = []
+
+    for file in files:
+        #do concatenation here to get full path 
+        full_path = os.path.join(root, file)
+        ext = os.path.splitext(file)[1]
+        selected_files.append(full_path)
+
+    return selected_files
+
+def build_recursive_dir_tree(path):
+    """
+    path    -    where to begin folder scan
+    """
+    selected_files = []
+
+    for root, dirs, files in os.walk(path):
+        selected_files += select_files(root, files)
+    return selected_files
+
+def force_symlink(file1, file2):
+    """
+    Creates a symlink, remove existing file if it exits
+    file1    -    file to link
+    file2    -    destination
+    """
+    print file1 + u'\t\u2192\t' + file2
+    try:
+        os.symlink(file1, file2)
+    except OSError, e:
+        if e.errno == errno.EEXIST:
+            if os.path.isdir(file2):
+                shutil.rmtree(file2)
+            else:
+                os.remove(file2)
+            os.symlink(file1, file2)
 
