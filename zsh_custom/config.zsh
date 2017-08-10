f3d5c19f17a5e1d944d05f13693dad209f3aed66
Daniel Nilsson <daniel.nilsson1989@gmail.com>
2017-08-10 14:06:43 +0200

    alias aws login
    Signed-off-by: Daniel Nilsson <daniel.nilsson1989@gmail.com>

--- zsh_custom/config.zsh
+++ zsh_custom/config.zsh
@@ -21,3 +21,6 @@ alias compsoer="composer"
 
 drc() { echo "\nRemoving docker containers\n"; for i in $(docker ps -aq); do docker rm -f $i; done }
 dri() { echo "\nRemoving docker images\n"; for i in $(docker images -q); do docker rmi $i; done }
+
+
+aws_login() { exec $(aws ecr get-login --no-include-email) }
