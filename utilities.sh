178f4344b4b409cafd5669b9bfc8eb84e553e7cb
Daniel Nilsson <daniel.nilsson1989@gmail.com>
2017-08-11 14:10:26 +0200

    renamed
    Signed-off-by: Daniel Nilsson <daniel.nilsson1989@gmail.com>

--- utilities.sh
+++ utilities.sh
@@ -7,5 +7,3 @@ gem install iStats
 # install HTTPie with the pip utility
 pip install httpie
 
-## Install the Fishshell Framework.
-curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
