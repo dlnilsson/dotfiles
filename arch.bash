3eae204048be5c22c4612a243300ea14439fcdc9
Daniel Nilsson <daniel.nilsson1989@gmail.com>
2019-05-08 20:25:12 +0200

    updated arch pacman list.
    Signed-off-by: Daniel Nilsson <daniel.nilsson1989@gmail.com>

--- arch.bash
+++ arch.bash
@@ -1,7 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# Ask for the administrator password upfront
+sudo -v
 
-pacman -S
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+sudo pacman -S \
   the_silver_searcher \
   awless \
   fzf \
@@ -15,3 +20,6 @@ pacman -S
   kbfs \
   ttf-fira-code \
   diff-so-fancy
+
+
+sudo pacman -R palemoon-bin
