7b87fdf6096693647986886a34fb51500331916c
Daniel Nilsson <daniel.nilsson1989@gmail.com>
2019-05-08 15:31:07 +0200

    listed arch packages.
    for convience.
    
    Signed-off-by: Daniel Nilsson <daniel.nilsson1989@gmail.com>

---
+++ arch.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+pacman -S
+  the_silver_searcher \
+  awless \
+  fzf \
+  go \
+  docker \
+  docker-compose \
+  neo-fetch \
+  xsel \
+  fzf \
+  keybase \
+  kbfs \
+  ttf-fira-code \
+  diff-so-fancy
