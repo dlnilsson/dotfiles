3c7608c15ea83de8f9c11a966445cef5226b110c
Daniel Nilsson <daniel.nilsson1989@gmail.com>
2019-09-09 16:31:40 +0200

    updated chrome history for linux
    Signed-off-by: Daniel Nilsson <daniel.nilsson1989@gmail.com>

--- zsh_custom/config.zsh
+++ zsh_custom/config.zsh
@@ -167,14 +167,19 @@ chistory() {
 	local cols sep
 	cols=$(( COLUMNS / 3 ))
 	sep='{::}'
-
-	cp -f ~/Library/Application\ Support/Google/Chrome/Default/History /tmp/h
+	local historyDB
+	local openCMD
+	case $( uname -s ) in
+	  Darwin) historyDB=~/Library/Application\ Support/Google/Chrome/Default/History openCMD=open;;
+	  *) historyDB=$HOME/.config/google-chrome/Default/History openCMD=xdg-open;;
+	esac
+	cp -f $historyDB /tmp/h
 
 	sqlite3 -separator $sep /tmp/h \
 	"select substr(title, 1, $cols), url
 	from urls order by last_visit_time desc" |
 	awk -F $sep '{printf "%-'$cols's  \x1b[36m%s\x1b[m\n", $1, $2}' |
-	fzf --ansi --multi | sed 's#.*\(https*://\)#\1#' | xargs open
+	fzf --ansi --multi | sed 's#.*\(https*://\)#\1#' | xargs $openCMD 2> /dev/null
 }
 goland() {
 	/usr/local/bin/goland .
