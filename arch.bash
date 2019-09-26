0fd67637ecd11ed251d0e1cc8e5b156189b535bb
Daniel Nilsson <daniel.nilsson1989@gmail.com>
2019-09-26 13:44:17 +0200

    update arch pkg install script.
    Signed-off-by: Daniel Nilsson <daniel.nilsson1989@gmail.com>

--- arch.bash
+++ arch.bash
@@ -6,56 +6,20 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.arch.bash` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# update
+echo "Updating pacman..."
 sudo pacman -Syy
 
-sudo pacman -S --noconfirm the_silver_searcher
-sudo pacman -S --noconfirm awless
-sudo pacman -S --noconfirm fzf
-sudo pacman -S --noconfirm go
-sudo pacman -S --noconfirm docker
-sudo pacman -S --noconfirm docker-compose
-sudo pacman -S --noconfirm neofetch
-sudo pacman -S --noconfirm xsel
-sudo pacman -S --noconfirm fzf
-sudo pacman -S --noconfirm keybase
-sudo pacman -S --noconfirm kbfs
-sudo pacman -S --noconfirm ttf-fira-code
-sudo pacman -S --noconfirm diff-so-fancy
-sudo pacman -S --noconfirm python-pip
-sudo pacman -S --noconfirm screen
-sudo pacman -S --noconfirm git-lfs
-sudo pacman -S --noconfirm jq
-sudo pacman -S --noconfirm mtr
-sudo pacman -S --noconfirm unzip
-sudo pacman -S --noconfirm py3status
-sudo pacman -S --noconfirm termite
-sudo pacman -S --noconfirm polybar
-sudo pacman -S --noconfirm rofi
-sudo pacman -S --noconfirm bind-tools
-sudo pacman -S --noconfirm adobe-source-code-pro-fonts
-sudo pacman -S --noconfirm noto-fonts-emoji
-sudo pacman -S --noconfirm ttf-nerd-fonts-symbol
-sudo pacman -S --noconfirm gnu-netcat
-sudo pacman -S --noconfirm whois
-sudo pacman -S --noconfirm exa
-sudo pacman -S --noconfirm imagemagick
-sudo pacman -S --noconfirm copyq
-sudo pacman -S --noconfirm kitty
-sudo pacman -S --noconfirm tilix
-# python-gnupg
+arch_packages="$HOME/.dotfiles/arch_packages"
+echo "Installing packages from $arch_packages"
+while IFS= read -r pkg; do
+	sudo pacman -S --noconfirm  $pkg
+done <"$arch_packages"
 
-
-yay ttf-font-awesome-4
-yay termsyn-font
-#yay ttf-iosevka
-yay feh
-yay betterlockscreen
-yay uthash
-yay deadd-notification-center
-yay notify-send.sh
-yay wpgtk
-yay rofi-calc
+aur_packages="$HOME/.dotfiles/aur_packages"
+echo "Installing AUR packages from $aur_packages"
+while IFS= read -r pkg; do
+	yay $pkg
+done <"$aur_packages"
 
 echo "Remove standard packages..."
 sudo pacman -R \
