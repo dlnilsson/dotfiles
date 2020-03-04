cc9922cb79fa8ff3187c0b01db83b1e9a8c153e4
Daniel Nilsson <daniel.nilsson1989@gmail.com>
2020-03-04 08:53:48 +0100

    bump
    Signed-off-by: Daniel Nilsson <daniel.nilsson1989@gmail.com>

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,34 +1,28 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u "$UID" -x polybar >/dev/null; do sleep 1; done
 
 
 sleep .5
 
-MONITOR="HDMI1"
+MONITOR="DP-1"
 
 
 if ! pgrep -x polybar; then
 
 	if type "xrandr"; then
-		MONITOR=$m polybar -c $HOME/.config/polybar/config.ini base &> /home/dln/logs/polybar.log &
+		MONITOR=$MONITOR polybar -c "$HOME/.config/polybar/config.ini" base &
 		for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-			# MONITOR=$m polybar --reload example &
-			# MONITOR=$m polybar --reload example &
-			# MONITOR=$m polybar -c $HOME/.config/polybar/config.ini secondary &> /home/dln/logs/polybar.log &
-			MONITOR=$m polybar -c $HOME/.config/polybar/config.ini -l info bottom &> /home/dln/logs/polybar.log &
+			MONITOR=$m polybar -c "$HOME"/.config/polybar/config.ini -l info bottom &
 		done
-	else
-		export MONITOR="HDMI1"
-		polybar -c $HOME/.config/polybar/config.ini base &> /home/dln/logs/polybar.log &
-		polybar -c $HOME/.config/polybar/config.ini bottom &> /home/dln/logs/polybar.log &
 	fi
 else
 	pkill -USR1 polybar
 fi
 
 echo "Bars launched..."
+#   &> /home/dln/logs/polybar.log
