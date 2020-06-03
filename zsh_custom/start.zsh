d29b46f12a93bd0cb4ef8588934d44787c1865f5
Daniel Nilsson <daniel.nilsson1989@gmail.com>
2020-06-03 15:32:22 +0200

    check if f exists
    Signed-off-by: Daniel Nilsson <daniel.nilsson1989@gmail.com>

--- zsh_custom/start.zsh
+++ zsh_custom/start.zsh
@@ -1,8 +1,16 @@
 
-fpath=($HOME/completions $fpath)
+if [ -d "$HOME/completions" ]; then
+    fpath=($HOME/completions $fpath)
+    source $HOME/completions/*.bash
+fi
+
+if [ -d "$HOME/lib/azure-cli" ]; then
+    source $HOME/lib/azure-cli/az.completion
+fi
+
+# if [ -d "$HOME/.cache/yay/azure-cli/src/azure-cli" ]; then
+# 	source "$HOME/.cache/yay/azure-cli/src/azure-cli/az.completion"
+# fi
 
 autoload -U +X compinit && compinit
 autoload -U +X bashcompinit && bashcompinit
-
-source $HOME/completions/*.bash
-source $HOME/lib/azure-cli/az.completion
