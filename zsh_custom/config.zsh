27835b27db0abf4913d4bf075d49affffb757084
Daniel Nilsson <daniel.nilsson1989@gmail.com>
2022-10-31 10:11:07 +0100

    custom bindings for fzf preview window

--- zsh_custom/config.zsh
+++ zsh_custom/config.zsh
@@ -13,7 +13,8 @@ export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
     --color=fg:#e5e9f0,bg:#3b4251,hl:#81a1c1
     --color=fg+:#eceff4,bg+:#4c566a,hl+:#8fbcbb
     --color=info:#eacb8a,prompt:#bf6069,pointer:#b48dac
-    --color=marker:#a3be8b,spinner:#b48dac,header:#a3be8b'
+    --color=marker:#a3be8b,spinner:#b48dac,header:#a3be8b
+	--bind page-up:preview-up,page-down:preview-down'
 
 # export TERMINAL=termite
 
