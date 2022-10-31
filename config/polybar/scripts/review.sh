6db483cd3eda98e8931e54e7ffb2879fa409404e
Daniel Nilsson <daniel.nilsson1989@gmail.com>
2022-10-31 12:51:17 +0100

    polybar show review requests

---
+++ config/polybar/scripts/review.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+
+GITHUB_NOTIFICATION="https://github.com/pulls/review-requested"
+
+main() {
+    count=$(gh search prs --review-requested=@me --draft=false --state=open --json url | jq '. | length')
+    if [[ $count -gt 0 ]]; then
+        echo -e "\uf12a Review requested: $count"
+    fi
+}
+
+
+case "${1:-""}" in
+	left_click)
+		echo "left"
+		sleep .1
+		xdg-open "$GITHUB_NOTIFICATION" &> /dev/null
+		exit 0
+	;;
+	*)
+		main
+esac
